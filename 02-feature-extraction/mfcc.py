e1669f590acee27a81517e5d87c3cabfc459b753
fengchengli <fengchengli@bytedance.com>
2021-02-25 00:56:42 +0800

    fbank and mfcc extraction code

--- 02-feature-extraction/mfcc.py
+++ 02-feature-extraction/mfcc.py
@@ -3,22 +3,22 @@ import numpy as np
 from scipy.fftpack import dct
 
 # If you want to see the spectrogram picture
-# import matplotlib
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
-# def plot_spectrogram(spec, note,file_name):
-#     """Draw the spectrogram picture
-#         :param spec: a feature_dim by num_frames array(real)
-#         :param note: title of the picture
-#         :param file_name: name of the file
-#     """ 
-#     fig = plt.figure(figsize=(20, 5))
-#     heatmap = plt.pcolor(spec)
-#     fig.colorbar(mappable=heatmap)
-#     plt.xlabel('Time(s)')
-#     plt.ylabel(note)
-#     plt.tight_layout()
-#     plt.savefig(file_name)
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+def plot_spectrogram(spec, note,file_name):
+    """Draw the spectrogram picture
+        :param spec: a feature_dim by num_frames array(real)
+        :param note: title of the picture
+        :param file_name: name of the file
+    """ 
+    fig = plt.figure(figsize=(20, 5))
+    heatmap = plt.pcolor(spec)
+    fig.colorbar(mappable=heatmap)
+    plt.xlabel('Time(s)')
+    plt.ylabel(note)
+    plt.tight_layout()
+    plt.savefig(file_name)
 
 
 #preemphasis config 
@@ -26,7 +26,7 @@ alpha = 0.97
 
 # Enframe config
 frame_len = 400      # 25ms, fs=16kHz
-frame_shift = 160    # 10ms, fs=15kHz
+frame_shift = 160    # 10ms, fs=16kHz
 fft_len = 512
 
 # Mel filter config
@@ -74,19 +74,46 @@ def get_spectrum(frames, fft_len=fft_len):
     spectrum = np.abs(cFFT[:,0:valid_len])
     return spectrum
 
-def fbank(spectrum, num_filter = num_filter):
+def fbank(spectrum, fs, num_filter = num_filter):
     """Get mel filter bank feature from spectrum
         :param spectrum: a num_frames by fft_len/2+1 array(real)
         :param num_filter: mel filters number, default 23
         :returns: fbank feature, a num_frames by num_filter array 
         DON'T FORGET LOG OPRETION AFTER MEL FILTER!
     """
-
-    feats=np.zeros(spectrum.shape[0], num_filter))
     """
         FINISH by YOURSELF
     """
-    return feats
+    mel_min = 0
+    mel_max = 2595 * np.log10(1 + fs / 2.0 / 700)
+    print('mel_max : ', mel_max)
+    mel_points = np.linspace(mel_min, mel_max, num_filter + 2) # n_filter个mel值均匀分布与最低与最高mel值之间
+    print('mel_points : ', mel_points)
+    hz_points = 700 * (10 ** (mel_points / 2595.0) - 1) # mel值对应回频率点，频率间隔指数化
+    print('hz_points: ', hz_points)
+    filter_edge = np.floor(hz_points * (fft_len + 1) / fs) # 对应到fft的点数比例上
+    print('filter_edge: ', filter_edge) ## 长度25
+
+    # 求mel滤波器系数(23, 257)
+    fbank = np.zeros((num_filter, int(fft_len / 2 + 1)))
+    for m in range(1, 1 + num_filter):
+        f_left = int(filter_edge[m - 1])
+        f_center = int(filter_edge[m])
+        f_right = int(filter_edge[m + 1])
+
+        for k in range(f_left, f_center):
+            fbank[m - 1, k] = (k - f_left) / (f_center - f_left)
+
+        for k in range(f_center, f_right):
+            fbank[m - 1, k] = (f_right - k) / (f_right - f_center)
+
+    # mel 滤波
+    # [num_frame, nfft/2 + 1] * [nfft/2 + 1, n_filter] = [num_frame, n_filter]
+    filter_banks = np.dot(spectrum, fbank.T)
+    filter_banks = np.where(filter_banks == 0, np.finfo(float).eps, filter_banks)
+    
+    filter_banks = 20 * np.log10(filter_banks) # dB
+    return filter_banks
 
 def mfcc(fbank, num_mfcc = num_mfcc):
     """Get mfcc feature from fbank feature
@@ -94,12 +121,11 @@ def mfcc(fbank, num_mfcc = num_mfcc):
         :param num_mfcc: mfcc number, default 12
         :returns: mfcc feature, a num_frames by num_mfcc array 
     """
-
-    feats = np.zeros((fbank.shape[0],num_mfcc))
     """
         FINISH by YOURSELF
     """
-    return feats
+    mfcc = dct(fbank, type=2, axis=1, norm='ortho')[:, 1:(num_mfcc+1)]
+    return mfcc
 
 def write_file(feats, file_name):
     """Write the feature to file
@@ -121,11 +147,11 @@ def main():
     signal = preemphasis(wav)
     frames = enframe(signal)
     spectrum = get_spectrum(frames)
-    fbank_feats = fbank(spectrum)
+    fbank_feats = fbank(spectrum, fs)
     mfcc_feats = mfcc(fbank_feats)
-    # plot_spectrogram(fbank_feats, 'Filter Bank','fbank.png')
+    plot_spectrogram(fbank_feats, 'Filter Bank','fbank.png')
     write_file(fbank_feats,'./test.fbank')
-    # plot_spectrogram(mfcc_feats.T, 'MFCC','mfcc.png')
+    plot_spectrogram(mfcc_feats.T, 'MFCC','mfcc.png')
     write_file(mfcc_feats,'./test.mfcc')
 
 if __name__ == '__main__':
